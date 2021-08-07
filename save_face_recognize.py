aa6eec9d5a5f35e12c211e4d437ffa34ffe1843b
Aman-a14 <73488475+Aman-a14@users.noreply.github.com>
2021-08-07 14:32:56 +0530

    Update save_face_recognize.py

--- save_face_recognize.py
+++ save_face_recognize.py
@@ -1,11 +1,14 @@
 import pickle
 import face_recognition
 import os
+import pickle
 import gtts
+
+from face_recognition.api import face_locations
 
 training_encodings = []
 Names = []
-image_dir =  r"C:\Users\91974\OneDrive\Desktop\all_other_stuff\Python\Open cv\known"
+image_dir =  r" *Your directory of known images* "
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
@@ -14,18 +17,16 @@ for root, dirs, files in os.walk(image_dir):
         person = face_recognition.load_image_file(path)
         encoding = face_recognition.face_encodings(person)[0]
         training_encodings.append(encoding)
-        if "_" in name:
-            name = name.split("_")[0]
         Names.append(name)
         print(name)
-with open('train.pkl','wb') as f:
-    pickle.dump(Names,f)
-    pickle.dump(training_encodings,f)
-print(Names)
-    
+        print(encoding)
+
 for name in Names:
     msg = name+"is at the door"
     tts = gtts.gTTS(msg,lang="en",tld = "co.uk")
     url = name+".mp3"
     tts.save(url)
 
+with open('train.pkl','wb') as f:
+    pickle.dump(Names,f)
+    pickle.dump(training_encodings,f)
